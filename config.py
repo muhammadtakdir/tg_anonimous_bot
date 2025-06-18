93df962cb4119a03be73a7e9e456ae63ce8f55c3
Muhammad Takdir <muhammad.takdir@gmail.com>
2025-06-18 18:50:30 +0700

    edit id tele

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ BOT_TOKEN = "12388kksdjdjkksdnsksdsdksd" #ganti ke token bot anda
 
 # ID chat admin (akan diisi otomatis saat admin pertama kali mengirim /start)
 # ADMIN_CHAT_IDS = set()  # Bisa support multi-admin
-PRE_APPROVED_ADMINS = [132224532]  # Chat ID admin yang diizinkan
+PRE_APPROVED_ADMINS = [11111111]  # Chat ID admin yang diizinkan
